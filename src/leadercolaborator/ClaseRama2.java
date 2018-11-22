137a4fff45e73f741f5b58af7c5906b832462e38
Samuel1418 <sgomezpintos@danielcastelao.org>
2018-11-22 09:14:58 +0100

    Primer commit rama 2

---
+++ src/leadercolaborator/ClaseRama2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leadercolaborator;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ClaseRama2 {
+    public void quepasa(){
+        System.out.println("Soy el colaborador Samuel1418");
+    }
+}
